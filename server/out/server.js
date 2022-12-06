55155d911adb5c84af34041b933414fa728d2c1c
DavTheDev20 <reiddavin@outlook.com>
2022-12-05 20:56:39 -0500

    Add new api route to register new user and create user model

--- server/out/server.js
+++ server/out/server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,20 +17,44 @@ dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const dbConnect_1 = __importDefault(require("./utils/dbConnect"));
+const user_1 = __importDefault(require("./models/user"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/react-auth-test-DB';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/react-auth-test-DB';
+const SALT_ROUNDS = 10;
+const JWT_SECRET = 'secret';
 (0, dbConnect_1.default)(MONGODB_URI);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, morgan_1.default)('dev'));
 app.use((0, cors_1.default)());
-app.get('/api/test', (req, res) => {
-    return res.status(200).json({
-        username: 'bobjones24',
-        email: 'bobjones24@yahoo.com',
-        password: 'password',
-    });
-});
+app.post('/api/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    if (!body.email || !body.username || !body.password)
+        return res.status(400).json({
+            success: false,
+            error: 'Please include email, username, and password in request body.',
+        });
+    try {
+        const salt = yield bcrypt_1.default.genSalt(SALT_ROUNDS);
+        const hash = yield bcrypt_1.default.hash(body.password, salt);
+        const newUser = new user_1.default({
+            email: body.email,
+            username: body.username,
+            password: hash,
+        });
+        newUser.save((err, user) => {
+            if (err)
+                return res.status(400).json({ success: false, error: err.message });
+            const jwt_token = jsonwebtoken_1.default.sign({ _id: user._id, email: user.email }, JWT_SECRET, { algorithm: 'HS256' });
+            return res.status(200).json({ success: true, token: jwt_token });
+        });
+    }
+    catch (err) {
+        return res.status(400).json({ success: false, error: err.message });
+    }
+}));
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,17 @@ dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import dbConnect from './utils/dbConnect';
+import User from './models/user';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/react-auth-test-DB';
+  process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/react-auth-test-DB';
+const SALT_ROUNDS = 10;
+const JWT_SECRET = 'secret';
 
 dbConnect(MONGODB_URI);
 
@@ -17,12 +22,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
 
-app.get('/api/test', (req, res) => {
-  return res.status(200).json({
-    username: 'bobjones24',
-    email: 'bobjones24@yahoo.com',
-    password: 'password',
-  });
+app.post('/api/register', async (req, res) => {
+  const body: { email: string; username: string; password: string } = req.body;
+
+  if (!body.email || !body.username || !body.password)
+    return res.status(400).json({
+      success: false,
+      error: 'Please include email, username, and password in request body.',
+    });
+
+  try {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hash = await bcrypt.hash(body.password, salt);
+    const newUser = new User({
+      email: body.email,
+      username: body.username,
+      password: hash,
+    });
+    newUser.save((err, user) => {
+      if (err)
+        return res.status(400).json({ success: false, error: err.message });
+
+      const jwt_token = jwt.sign(
+        { _id: user._id, email: user.email },
+        JWT_SECRET,
+        { algorithm: 'HS256' }
+      );
+
+      return res.status(200).json({ success: true, token: jwt_token });
+    });
+  } catch (err: any) {
+    return res.status(400).json({ success: false, error: err.message });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
